9e8973eeea038f466373681f62d08501fb8be226
miguelhat09 <miguelangel_hsg@hotmail.com>
2020-11-26 17:34:28 -0500

    borrado

--- wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-<<<<<<< HEAD
-// Write your Javascript code.
-=======
-// Write your JavaScript code.
->>>>>>> 86ebea09369da69504054bbe768b5fbe9955545b
